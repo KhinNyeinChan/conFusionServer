5c2f75a1a51717916b4dfc606cac0290738bc20f
KhinNyeinChan <poe2161999@gmail.com>
2021-01-26 18:02:27 +0630

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId : '316720779770774',
+        clientSecret : '54e37103a0419f95712de989a4460319'
+    }
 };
--- model/users.js
+++ model/users.js
@@ -11,6 +11,7 @@ var User = new Schema({
         type: String,
         default: ''
     },
+    facebookId : String,    //facebookId passed in access token
     admin: {
         type: Boolean,
         default: false
